2eef5235ed4d665c8eadd5b9a670b17a2865db17
Andresdzem <91997112+Andresdzem@users.noreply.github.com>
2024-12-16 21:43:36 -0300

    Add files via upload

---
+++ JS/validacionContacto.js
@@ -0,0 +1,45 @@
+
+
+const botonCotizar = document.getElementById ('botonCotizar');
+const formulario = document.getElementById ('formularioCotizar');
+
+
+
+
+formulario.addEventListener('submit', evento  =>{
+        evento.preventDefault();
+
+ const nombre = document.getElementById('nombre').value.trim()
+ const email = document.getElementById('inputEmail3').value.trim()
+ const mensaje = document.getElementById('mensaje')
+ const errorNombre = document.getElementById('errorNombre')
+ const errorMail = document.getElementById('errorMail')
+ const errorMensaje = document.getElementById('errorMensaje')
+
+
+ let formularioValido = true;
+
+if (nombre === ""){
+    errorNombre.classList.remove('d-none')
+    formularioValido = false
+}   else{
+    errorNombre.classList.add('d-none')
+}
+
+const emailRegex = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+
+if (!emailRegex.test(email)){
+    errorMail.classList.remove('d-none')
+    formularioValido = false
+}
+else{
+    errorMail.classList.add('d-none')
+}
+
+if (mensaje.value.trim() === "") {
+    errorMensaje.classList.remove('d-none')
+    formularioValido = false
+}   else{
+    errorMensaje.classList.add('d-none')
+}
+})
